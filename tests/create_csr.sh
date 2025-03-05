62dfee11288a0f9fb2f7ee9e9beb6834e3d47adf
Miguel Miranda <miguel.miranda@sapo.pt>
2025-03-05 05:25:33 +0000

    Support pre-made CSR

--- tests/create_csr.sh
+++ tests/create_csr.sh
@@ -66,9 +66,28 @@ if [[ "$CSR_MODE" == "0" ]]; then
 fi
 
 
+key_cmd=""
+key_algorithm=$(echo ${ALGORITHM} | cut -d ':' -f1)
+
+if [[ "${key_algorithm}" == "RSA" ]]; then
+    key_opt=$(echo ${ALGORITHM} | cut -d ':' -f2)
+    # Generate key
+    key_cmd="openssl genpkey -algorithm ${key_algorithm} -pkeyopt rsa_keygen_bits:${key_opt} 2>/dev/null"
+elif [[ "${key_algorithm}" == "EC" ]]; then
+    key_opt=$(echo ${ALGORITHM} | cut -d ':' -f2)
+    # Generate key
+    key_cmd="openssl genpkey -algorithm ${key_algorithm} -pkeyopt ec_paramgen_curve:${key_opt} 2>/dev/null"
+elif [[ "${key_algorithm}" == "ED25519" ]]; then
+    # Generate key
+    key_cmd="openssl genpkey -algorithm ${key_algorithm} 2>/dev/null"
+fi
+
+# Change "#" with ${THREAD}
+changed_subject=$(echo "$SUBJECT" | sed "s/#/\${THREAD}/g")
+
 # Create CSR
 #csr_cmd="openssl req -new -key <(${key_cmd}) -subj /CN=enduser${THREAD}/O=enduser${THREAD}_org/C=PT"
-csr_output=$(generate_csr_cmd "$ALGORITHM" "$THREAD" "$SUBJECT")
+csr_cmd="openssl req -new -key <($key_cmd) -subj ${changed_subject}"
 echo -n "-----BEGIN CERTIFICATE REQUEST-----\n"
-eval ${csr_output} | grep -v "CERTIFICATE REQUEST" | tr -d '\n'
+eval ${csr_cmd} | grep -v "CERTIFICATE REQUEST" | tr -d '\n'
 echo -n "\n-----END CERTIFICATE REQUEST-----"
